30971d3b19b11065cc22fea1aaabdf026b6049d6
Quan Nguyen <157680058+quan-nguyen-2@users.noreply.github.com>
2024-08-02 17:10:52 -0500

    Add files via upload

---
+++ express-messagely/routes/messages.js
@@ -0,0 +1,82 @@
+/** GET /:id - get detail of message.
+ *
+ * => {message: {id,
+ *               body,
+ *               sent_at,
+ *               read_at,
+ *               from_user: {username, first_name, last_name, phone},
+ *               to_user: {username, first_name, last_name, phone}}
+ *
+ * Make sure that the currently-logged-in users is either the to or from user.
+ *
+ **/
+
+
+/** POST / - post message.
+ *
+ * {to_username, body} =>
+ *   {message: {id, from_username, to_username, body, sent_at}}
+ *
+ **/
+
+
+/** POST/:id/read - mark message as read:
+ *
+ *  => {message: {id, read_at}}
+ *
+ * Make sure that the only the intended recipient can mark as read.
+ *
+ **/
+
+const express = require("express");
+const Message = require("../models/message");
+const { ensureLoggedIn } = require("../middleware/auth");
+const router = new express.Router();
+
+/** GET /:id - get detail of message. */
+router.get("/:id", ensureLoggedIn, async function (req, res, next) {
+  try {
+    const message = await Message.get(req.params.id);
+
+    if (message.from_user.username !== req.user.username && message.to_user.username !== req.user.username) {
+      throw new Error("Unauthorized");
+    }
+
+    return res.json({ message });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** POST / - post message. */
+router.post("/", ensureLoggedIn, async function (req, res, next) {
+  try {
+    const { to_username, body } = req.body;
+    const message = await Message.create({
+      from_username: req.user.username,
+      to_username,
+      body
+    });
+    return res.json({ message });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** POST/:id/read - mark message as read */
+router.post("/:id/read", ensureLoggedIn, async function (req, res, next) {
+  try {
+    const message = await Message.get(req.params.id);
+
+    if (message.to_user.username !== req.user.username) {
+      throw new Error("Unauthorized");
+    }
+
+    const updatedMessage = await Message.markRead(req.params.id);
+    return res.json({ message: updatedMessage });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
